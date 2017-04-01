2e17012e0958eb30e94b849f6f52208bc6d1115a
Yunus Emre Demirbulut <y.emre.demirbulut@gmail.com>
2017-04-01 23:48:49 +0300

    e, m, i letters added

--- recognizer.py
+++ recognizer.py
@@ -17,7 +17,11 @@ def image2Vector(path):
 
 
 X = np.zeros((20, 901)) # Array full of input
-paths = np.array(['d_1.png', 'd_2.png', 'd_3.png', 'd_4.png', 'd_5.png']) # Train paths
+# Train paths
+paths = np.array(['d_1.png', 'd_2.png', 'd_3.png', 'd_4.png', 'd_5.png',
+                  'e_1.png', 'e_2.png', 'e_3.png', 'e_4.png', 'e_5.png',
+                  'm_1.png', 'm_2.png', 'm_3.png', 'm_4.png', 'm_5.png',
+                  'i_1.png', 'i_2.png', 'i_3.png', 'i_4.png', 'i_5.png'])
 for i in xrange(0, paths.size):
     a = image2Vector('images/train/' + paths[i])
     X[i] = a
